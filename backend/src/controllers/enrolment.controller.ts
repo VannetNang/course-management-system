e5155e77002611a1c6870bb48b03b9797c8f4f30
VannetNang <NangVannet59@gmail.com>
2026-02-23 15:03:17 +0700

    feat(payment): done adding create transaction for users

--- backend/src/controllers/enrolment.controller.ts
+++ backend/src/controllers/enrolment.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { prisma } from '../config/db';
+import { COUNTRY, CURRENCY, KHQR, TAG } from 'ts-khqr';
+import config from '../config/config';
 
 // @desc    show course's price summary   (PUBLIC)
 // @Route   GET   /api/enrolments/summary/:id
@@ -56,16 +58,51 @@ export const getSummary = async (
 };
 
 // @desc    show QR CODE   (AUTH ONLY)
-// @Route   GET   /api/enrolments/checkout
+// @Route   GET   /api/enrolments/checkout/:id
 export const createTransaction = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
   try {
+    const { id } = req.params as { id: string };
+
+    // Find existing course
+    const course = await prisma.course.findUnique({
+      where: { id: id },
+    });
+
+    // If not found
+    if (!course) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'Course not found',
+      });
+    }
+
+    // Else, create transaction
+    const result = KHQR.generate({
+      tag: TAG.INDIVIDUAL,
+      accountID: config.bakong_account_id,
+      merchantName: config.bakong_merchant_name,
+      merchantCity: config.bakong_merchant_city,
+      currency: CURRENCY.KHR,
+      amount: Number(course.price),
+      countryCode: COUNTRY.KH,
+      expirationTimestamp: Date.now() + 1 * 60 * 1000, // 1 minute
+      additionalData: {
+        storeLabel: config.bakong_store_label,
+        terminalLabel: config.bakong_terminal_label,
+      },
+    });
+
     res.status(200).json({
       status: 'success',
       message: 'Course summary retrieved successfully',
+      data: {
+        qr: result.data?.qr,
+        md5: result.data?.md5,
+      },
     });
   } catch (error) {
     next(error);
